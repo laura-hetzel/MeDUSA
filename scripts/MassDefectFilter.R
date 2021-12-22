a0ad33e938088615516333247330ce930955b6e4
Pascal <p.maas@lacdr.leidenuniv.nl>
2021-12-22 11:05:36 +0100

    Update MassDefectFilter.R
    Latest version, sorry Klara!

--- scripts/MassDefectFilter.R
+++ scripts/MassDefectFilter.R
@@ -31,52 +31,62 @@ mdf_function <- function(dataframe){
   MD_df
 }
 
-#' inclusion list read in
-in_list_data <- "hmdb_inclusions_list_pos_McMillan.txt"
-
-#' set mass accuracy (in Daltons) for inclusion list matching
-ma <- 0.01
 
 
-#' integrating the cutoff line in the data
-f <- function(x){
-  y <- 0.00112 * x + 0.01953    # cut-off linear equation
-  return(y)
+
+#' @title
+#' @param
+create_fit_df <- function(MD_df){
+  fit <- as.data.frame(apply(MD_df[,1], 1, function(row){
+    y <- 0.00112 * row + 0.01953
+    return(y)
+  }))
+
+  colnames(fit) <- "mz"
+  fit
 }
 
-#MD_m <- matrix(MD_df[,7])
-
-fit <- as.data.frame(apply(MD_df[,1],1,f))
-colnames(fit) <- "mz"
-
-#' Inclusion list creation
-#' reproduced from the MacMillan salt cluster removal paper
-
-hmdb_list <- read.table(in_list_data,  header = T, check.names = F,
-              row.names = 1, sep = "\t")
+plot_mono_mass <- function(MD_df, title){
+  plot(MD_df$MONO_MASS, MD_df$MD, cex.axis = 0.8,
+       col = alpha("black", 0.5), pch = 20, cex = 0.8,
+       ylim = c(0,1), xlim = c(50,1200), ylab = "MD", xlab = "m/z",
+       main = title, cex.lab = 0.8, cex.main = 0.8)
+}
 
 
-#find masses in inclusion list in MD_df
-in_list <- hmdb_list[which(hmdb_list$"inclusion" == "y"),]                  #if in the input data inclusion list is put to y (yes) it is included here
-cmp <- function(MD_df, in_list, cutoff=ma){abs(in_list - MD_df) <= cutoff}  #compare compounds from from inclusion list
-match <- which(outer(in_list$mz, MD_df$MONO_MASS, cmp), arr.ind = TRUE)     # assign comparable values a logical TRUE value
-fc <- factor(match[,2])                                                     # make a factor out of the column 2
-I <- rownames(MD_df)[as.numeric(levels(fc))]                               # return row names of TRUE rows
+filter_function <- function(MD_df){
+  #' Inclusion list creation
+  #' reproduced from the MacMillan salt cluster removal paper
+  #'
+  #' #' inclusion list read in
+  in_list_data <- "hmdb_inclusions_list_pos_McMillan.txt"
 
-#keep rows in MD_df if m/z defect is less than or equal to fitted value OR value is in inclusion list
+  #' set mass accuracy (in Daltons) for inclusion list matching
+  ma <- 0.01
 
-filtered <- MD_df[which((MD_df$"MD" <= fit$"mz") | rownames(MD_df) %in% I),]
-
-#' plotting results
-plot(MD_df$"MONO_MASS", MD_df$"MD", cex.axis = 0.8,
-     col = alpha("black", 0.5), pch = 20, cex = 0.8,
-     ylim = c(0,1), xlim = c(50,1200), ylab = "MD", xlab = "m/z",
-     main = "Raw", cex.lab = 0.8, cex.main = 0.8)
+  hmdb_list <- read.table(in_list_data,  header = T, check.names = F,
+                          row.names = 1, sep = "\t")
 
 
-plot(filtered$"MONO_MASS", filtered$"MD", cex.axis = 0.8,
-     col = alpha("black", 0.5), pch = 20, cex = 0.8,
-     ylim = c(0,1),xlim = c(50,1200), ylab = "MD", xlab = "m/z",
-     main = "Filtered", cex.lab = 0.8, cex.main = 0.8)
+  #find masses in inclusion list in MD_df
+  in_list <- hmdb_list[which(hmdb_list$"inclusion" == "y"),]                  #if in the input data inclusion list is put to y (yes) it is included here
+  cmp <- function(MD_df, in_list, cutoff=ma){abs(in_list - MD_df) <= cutoff}  #compare compounds from from inclusion list
+  match <- which(outer(in_list$mz, MD_df$MONO_MASS, cmp), arr.ind = TRUE)     # assign comparable values a logical TRUE value
+  fc <- factor(match[,2])                                                     # make a factor out of the column 2
+  I <- rownames(MD_df)[as.numeric(levels(fc))]                               # return row names of TRUE rows
+
+  #keep rows in MD_df if m/z defect is less than or equal to fitted value OR value is in inclusion list
+
+  filtered <- MD_df[which((MD_df$"MD" <= fit$"mz") | rownames(MD_df) %in% I),]
+  filtered
+}
 
 
+pipeline <- function(file = NULL){
+  dataframe <- get_hmdb_file(file)
+  md_df <- mdf_function(dataframe)
+  fit <- create_fit_df(md_df)
+  plot_mono_mass(md_df)
+  filtered_df <- filter_function(md_df)
+  plot_mono_mass(filtered_df)
+}
