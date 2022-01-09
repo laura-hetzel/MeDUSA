4aaf1fe6d283f40e58e498a8e56f6f139544e2a3
Pascal <p.maas@lacdr.leidenuniv.nl>
2022-01-09 15:37:21 +0100

    Conversion of package using ABStools

--- ci.R
+++ ci.R
@@ -1,7 +1,7 @@
 setwd("sumR")
 
 install_if_needed <- function(package_to_install){
-  if (!package_to_install %in% installed.packages()) {
+  if (!package_to_install %in% installed.packages()){
     install.packages(package_to_install)
     print(sprintf("Installed %s", package_to_install))
   } else {
@@ -11,20 +11,17 @@ install_if_needed <- function(package_to_install){
 
 ci_setup <- function(){
   options(repos = structure(BiocManager::repositories()))
-  install_if_needed("devtools")
   install_if_needed("packrat")
-  packrat::init(restart = FALSE,
-                options = list(
-                  external.packages = c(
-                    "devtools", "roxygen2",
-                    "remotes", "digest"
-                  )
-                ))
+  packrat_options <- list(external.packages = c(
+      "devtools", "roxygen2","remotes", "digest")
+  )
+  packrat::init(restart = FALSE, options = packrat_options)
 }
 
 ci_check <- function(){
   if (length(list.files(path = "R") > 0)) {
     ci_setup()
+    install_if_needed("devtools")
     devtools::check(error_on = "error")
   }
 }
