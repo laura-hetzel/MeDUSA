e9ef5ca21336ac358bc585d929b5279e719161c2
Pascal <p.maas@lacdr.leidenuniv.nl>
2022-01-11 19:01:22 +0100

    Update ci.R

--- ci.R
+++ ci.R
@@ -12,10 +12,11 @@ install_if_needed <- function(package_to_install){
 ci_setup <- function(){
   options(repos = structure(BiocManager::repositories()))
   install_if_needed("packrat")
-  packrat::init(restart = FALSE)
-  packrat::restore()
+  if (!dir.exists("packrat")) {
+    packrat::init(restart = FALSE)
+  }
+  devtools::install(upgrade = F)
   packrat::snapshot()
-
 }
 
 ci_check <- function(){
