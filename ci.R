4a90380e91e638a5645bde100210e6957f27e574
Pascal <p.maas@lacdr.leidenuniv.nl>
2022-01-11 23:05:16 +0100

    Update ci.R

--- ci.R
+++ ci.R
@@ -10,6 +10,13 @@ install_if_needed <- function(package_to_install){
 }
 
 ci_setup <- function(){
+  if (!dir.exists("cache")) {
+    dir.create("cache", showWarnings = F)
+    folder <- sprintf("%s/cache", getwd())
+    print(sprintf("creating cache folder at %s", folder))
+    .libPaths(folder)
+  }
+  print(.libPaths()[1])
   options(repos = structure(BiocManager::repositories()))
   install_if_needed("devtools")
   devtools::install(upgrade = F)
