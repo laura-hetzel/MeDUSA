85884543920a33f7853522077b4e715ecf7e8740
klabla <klara.blaschitz@umail.leidenuniv.nl>
2022-01-13 11:05:21 +0100

    Update 13.01.22

--- scripts/MassDefectFilter.R
+++ scripts/MassDefectFilter.R
@@ -11,6 +11,7 @@ get_hmdb_file <- function(file){
   #} else {
     #' Downloading the data and converting it to a data frame
     dataframe <- read_delim("end.metabolites.txt", delim = ",")            #82.595 rows as should be
+    colnames(dataframe) <- c("HMDB_ID", "Name", "SMILES", "INCHIKEY", "Chemical_Formula", "Average_Mass", "mz")
     #removing rows with no Mass data
     dataframe <- na.omit(dataframe)
   #}
@@ -24,8 +25,7 @@ get_hmdb_file <- function(file){
 #' @export
 get_data <- function(file){
   #' Downloading the experimental data and converting it to a data frame
-  ex_data_df <- read_delim("889C0121170_NS_005_NEG_7_cvd_d5_pm.txt", col_names = F)
-  colnames(ex_data_df) <- c("p", "MONO_MASS", "l", "Intesity","e")
+  ex_data_df <- read_delim("peaks.csv", col_names = T)
   return(ex_data_df)
 }
 
@@ -34,7 +34,7 @@ get_data <- function(file){
 #' @importFrom dplyr mutate
 #' @export
 mass_defect_calculation <- function(dataframe){
-  mz_col <- dataframe$MONO_MASS
+  mz_col <- dataframe$mz
   MD <- mz_col %% 1
   #to add the MD to the new data frame
   MD_df <- dplyr::mutate(dataframe, MD)
@@ -46,7 +46,7 @@ mass_defect_calculation <- function(dataframe){
 #' @param dataframe MD_df obtained from the HMDB using `mass_defect_function`
 #' @export
 make_filter_list <- function(MD_df){
-  filter_list <- as.data.frame(apply(MD_df[,"MONO_MASS"], 1, function(row){
+  filter_list <- as.data.frame(apply(MD_df[,"mz"], 1, function(row){
     y <- 0.00112 * row + 0.01953
     return(y)
   }))
@@ -81,7 +81,7 @@ MD_filter <- function(MD_df, filter_list, incl_list){
   #find masses in inclusion list in MD_df
   incl_list_filtered <- incl_list[which(incl_list$"inclusion" == "y"),]                  #if in the input data inclusion list is put to yes it is included here
   cmp <- function(MD_df, incl_list_filtered, cutoff = ma){abs(incl_list_filtered - MD_df) <= cutoff}  #compare compounds from inclusion list to the data
-  match <- which(outer(incl_list_filtered$mz, MD_df$MONO_MASS, cmp), arr.ind = TRUE)     # assign comparable values a logical TRUE value
+  match <- which(outer(incl_list_filtered$mz, MD_df$mz, cmp), arr.ind = TRUE)     # assign comparable values a logical TRUE value
   filtered_rows <- rownames(MD_df)[as.numeric(levels(factor(match[,2])))]                               # return row names of TRUE rows
   #keep rows in MD_df if m/z defect is less than or equal to fitted value OR value is in inclusion list
   filtered_df <- MD_df[which((MD_df$MD <= filter_list$mz) | rownames(MD_df) %in% filtered_rows),]
@@ -93,7 +93,7 @@ MD_filter <- function(MD_df, filter_list, incl_list){
 #' @param dataframe filtered_MD_df obtained from the HMDB using `MD_filter`
 #' @export
 plot_mz_MD <- function(MD_df, title){
-  plot(MD_df$MONO_MASS, MD_df$MD, cex.axis = 0.8,
+  plot(MD_df$mz, MD_df$MD, cex.axis = 0.8,
        col = alpha("black", 0.5), pch = 20, cex = 0.8,
        ylim = c(0,1), xlim = c(50,1200), ylab = "MD", xlab = "m/z",
        main = title, cex.lab = 0.8, cex.main = 0.8)
@@ -103,7 +103,7 @@ plot_mz_MD <- function(MD_df, title){
 #' @param dataframe filtered_MD_df obtained from `MD_filter`
 result_output <- function(filtered_df){
   #write filtered_df in csv 
-  write.csv(filtered_df,"filtered_data.csv")
+  write.csv(filtered_df,"filtered_data.csv", row.names = F)
 }
 
 pipeline <- function(file = NULL){
