05272c6f31d3b3864770a3b40a2eb04e26dc27f7
klabla <klara.blaschitz@umail.leidenuniv.nl>
2022-01-19 17:30:59 +0100

    bin alignment testing set up
    data input works and align_check works

--- scripts/Bin_alignment.R
+++ scripts/Bin_alignment.R
@@ -1,14 +1,4 @@
-#' @title Import experimental data
-#' @param file String location of the experimental file
-#' @importFrom stats na.omit
-#' @importFrom readr read_delim
-#' @export
-get_data <- function(file){
-  # Downloading the experimental data and converting it to a data frame
-  ex_data_df <- read_delim("test.txt", col_names = T)
-  colnames(ex_data_df) <- c("x","mz","y", "Intesity", "z")
-  return(ex_data_df)
-}
+
 
 #' @title ppm calculation 
 #' @description ppm_calc calculated the parts per million error between two different masses
@@ -27,13 +17,15 @@ ppm_calc <- function(mass1, mass2) {
 #' 1- Dataframe of 1 column containing the ppm error values
 #' 2- boxplot of the ppm erro values with xcoords zoomed in to -20,0 (default)
 #' 3- table of summary stats of ppm error values
-#' @examples 
+#' @importFrom dplyr
+#' @importFrom tibble as_tibble_col
+#' @importFrom ggplot2 ggplot
 #' @export
 align_check <- function(data_frame_fn, xcoords = c(-20, 0)) {
   odd_ind_fn <- seq(3, length(data_frame_fn$mz), 2)
   even_ind_fn <- seq(2, length(data_frame_fn$mz),2)
   ppm_err_fn <- ppm_calc(data_frame_fn$mz[even_ind_fn], data_frame_fn$mz[odd_ind_fn]) %>% 
-    as_tibble_col( column_name = "ppm_error") 
+    as_tibble_col(column_name = "ppm_error") 
   ppm_err_plot_fn <- ggplot(ppm_err_fn, aes(x = ppm_error)) +
     geom_boxplot() +
     coord_cartesian(xlim = xcoords) +
@@ -48,11 +40,6 @@ align_check <- function(data_frame_fn, xcoords = c(-20, 0)) {
   return(ppm_err_list)
 }
 
-#-------------------------------------------
-#' @title Check if aligned or not JUST ME NOW
-#' @param datdaframe ex_data_df obtained from the  `get_data`
-check_alignment <- align_check(get_data(file))
-#-------------------------------------------
 
 #' @title Deletion of unwanted samples  
 #' @description binning dependency 1
@@ -60,8 +47,9 @@ check_alignment <- align_check(get_data(file))
 #' delete any sample that is kicked out by the tolerance 
 #' @param mass 
 #' @param samples
+#' @param intensities
 #' @param tolerance
-condition <- function(mass, samples, tolerance) {
+condition <- function(mass, intensities, samples, tolerance) {
   if (anyDuplicated(samples)) {
     return(NA)
   }
@@ -120,13 +108,14 @@ binning <- function(mass, intensities, samples, tolerance){
 
 #' @title Binning of the peaks 
 #' @description binPeaks function! This needs the two functions above
+#' @param list of dataframes 
 binPeaks <- function(l, ppm = 5) {
   nn <- sapply(l, nrow)
   nonEmpty <- nn != 0L
   samples <- rep.int(seq_along(l), nn)
-  mass <- unname(unlist((lapply(l[nonEmpty], function(x) x$mz)), recursive = FALSE, use.names = FALSE))
+   mass <- unname(unlist((lapply(l[nonEmpty], function(x) x$mz)), recursive = FALSE, use.names = FALSE))
   intensities <- unlist(lapply(l[nonEmpty], function(x) x$intensity), recursive = FALSE, use.names = FALSE)
-  s <- sort.int(mass, index.return = TRUE)
+  s <- sort.int(mass, index.return = TRUE) # sort vector based on masses lowest to highest 
   mass <- s$x
   intensities <- intensities[s$ix]
   samples <- samples[s$ix]
@@ -143,3 +132,56 @@ binPeaks <- function(l, ppm = 5) {
   }, p = l[nonEmpty], i = lIdx, MoreArgs = NULL, SIMPLIFY = FALSE, USE.NAMES = FALSE)
   return(l)
 }
+
+#-------------------------------------------
+##testing 
+#-------------------------------------------
+
+binPeaks(l)
+
+t <- binPeaks(l)
+t <- l$df1
+
+align_check(t)
+
+
+
+
+##getting the data 
+
+#' @title Import experimental data
+#' @param file String location of the experimental file
+#' @importFrom stats na.omit
+#' @importFrom readr read_delim
+#' @export
+get_data <- function(file){
+  # Downloading the experimental data and converting it to a data frame
+  ex_data_df <- read_delim(file, col_names = T)
+  colnames(ex_data_df) <- c("x","mz","y", "intensity", "z")
+  ex_data_df <- ex_data_df[,c("mz","intensity")]
+  return(ex_data_df)
+}
+
+create_df_list <- function(df1,df2, df3, df4, df5, df6, df7, df8, df9, df10){
+  l <- mget(ls(pattern = "df[1,2,3,4,5,6,7,8,9,10]"))
+}
+  
+
+dfs <- function(file) {
+  df1 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test1.txt")
+  df2 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test2.txt")
+  df3 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test3.txt")
+  df4 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test4.txt")
+  df5 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test5.txt")
+  df6 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test6.txt")
+  df7 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test7.txt")
+  df8 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test8.txt")
+  df9 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test9.txt")
+  df10 <- get_data("/Users/klarab/Documents/GitHub/sum-r/scripts/bin_test/test10.txt")
+  l <- create_df_list(df1,df2, df3, df4, df5, df6, df7, df8, df9, df10)
+}
+l <- dfs(file)
+
+
+
+
