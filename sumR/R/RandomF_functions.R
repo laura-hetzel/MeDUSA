da99c2e40c4791efd23ca44ef03a8122fe26cc75
Eman-Mu <eman.muhammad@hotmail.com>
2022-02-20 19:27:33 +0000

    Update RandomF_functions.R

--- sumR/R/RandomF_functions.R
+++ sumR/R/RandomF_functions.R
@@ -122,123 +122,80 @@ rf_group <- function(k, data, class1 , class2 , seed){
 
 # Cross validation
 
-#' @title feature selection
+#' @title feature selection by cross validation
 #' @description This function apply random forest model to the splitted subsets and extract the important variable from each
-#' model and the roc and auc for each model
+#' model and the roc and auc for each model 
 #' @param training_set the training set
 #' @param test_set the test set
-#' @param k number of subsets splitted
+#' @param k number of subsets splitted 
 #' @param seed global seed for reproducible results
 #' @param class1 sample group 1 name for renaming
 #' @param class2 sample group 2 name for renaming
 #' @param n number of imp features to be selected in each model
-#' @importFrom randomForest randomForest
-#' @importFrom randomForest importance
-#' @importFrom dplyr select
-#' @importFrom caret rfe
-#' @importFrom tibble rownames_to_column
-#' @importFrom tibble column_to_rownames
-#' @importFrom dplyr arrange
-#' @importFrom dplyr desc
-#' @importFrom dplyr filter_all
-#' @importFrom caret confusionMatrix
-#' @importFrom pROC roc
-#' @importFrom pROC auc
+#' @importFrom randomForest randomForest importance
+#' @importFrom caret rfe confusionMatrix
+#' @importFrom dplyr select arrange desc
+#' @importFrom pROC roc auc
 randomForest_CV<- function (training_set,test_set,class1,class2,k,seed,n){
-
-
-
-  rflist <- rf_group(k = k,data=training_set,class1,class2, seed = seed)
+  rflist <- rf_group(k = k,data=training_set,class1,class2, seed = seed)  
   data<-training_set
-
   cfmatrix <- list() # store the confusion matrix table of each group
   auc_all <- list()  # store the auc of each group
   roc_all <- list() # store the ROC curve of each group
   cfmatrix_summary <- list() # store the confusion matrix summary of each group
   imp<-list()
-  set.seed(seed)
+  imp_all<-data.frame()
   for (i in 1:k){
-    train <- data[-rflist[[i]],]
+    train <- data[-rflist[[i]],]  
     test <- data[rflist[[i]],]
+    set.seed(seed)
     model<-randomForest(x=(train %>% dplyr::select(-samples)),
                         y=train$samples,data=train,ntree=500,importance = T,proximity = T)
-
     tmpimp <- as.data.frame(importance(model))
     tmpimp<-rownames_to_column(tmpimp,"mz")
     tmpimp <- tmpimp %>% arrange(desc(MeanDecreaseAccuracy))
-
     imp [[i]] <-  tmpimp
     mypred <- predict(model, newdata = test, type = "class")
     mypred<-as.data.frame(cbind("observed"=(test$samples),"predict"=mypred))
-
     xtab <- table(as.factor(mypred$predict), mypred$observed)
     print(confusionMatrix(xtab))
     cfmatrix_summary[[i]] <- confusionMatrix(xtab)
     cfmatrix[[i]] <- as.matrix(table(mypred$predict,mypred$observed))
-
-
-
     roc<- roc(mypred$observed,mypred$predict)
     auc<- auc(roc)
-
     auc_all[[i]] <- auc
     roc_all[[i]] <- roc
-
-
-
+    imp[i]<-head(imp[[i]],n=n)
+    imp_all<-rbind(imp_all,imp[i])
   }
+  colnames(imp_all)<-"mz"
+  return(list("auc_all"=auc_all,"roc_all"=roc_all,"imp"=imp,
+              "cfmatrix_summary"=cfmatrix_summary,"imp_all"=imp_all))
+}
 
 
-  imp_1<-head(imp[[1]],n=n)
-  imp_2<-head(imp[[2]],n=n)
-  imp_3<-head(imp[[3]],n=n)
-  imp_4<-head(imp[[4]],n=n)
-  imp_5<-head(imp[[5]],n=n)
-
-  imp_all<-rbind(imp_1,imp_2,imp_3,imp_4,imp_5)
-
-  training_set$samples<-paste(training_set$samples,1:nrow(training_set),sep="_") ## we just do this step so we can move the samples to row names ..
-  rownames(training_set)<-NULL
-  training_set<-column_to_rownames(training_set,"samples")
-
-  training_set_t<-as.data.frame(t(training_set))
-
-  training_set_t<-rownames_to_column(training_set_t,"mz")
-
-  imp_var<-training_set_t %>% filter_all(any_vars(. %in% imp_all$mz)) ## obtaining the intensities of the all imp mz
+#' @title Cross-validation data
+#' @param data the training set or test set
+#' @param imp_all the dataframe of all imp m/z from the list of randomForest_CV function
+#' @param class1 sample group 1 name for renaming
+#' @param class2 sample group 2 name for renaming
+#' @importFrom dplyr filter_all any_vars
+#' @importFrom tibble rownames_to_column column_to_rownames
+CV_data<-function(data,imp_all,class1,class2){
+  data$samples<-paste(data$samples,1:nrow(data),sep="_") ## we just do this step so we can move the samples to row names .. 
+  rownames(data)<-NULL
+  data<-column_to_rownames(data,"samples")
+  data_t<-as.data.frame(t(data))
+  data_t<-rownames_to_column(data_t,"mz")
+  imp_var<-data_t %>% filter_all(any_vars(. %in% imp_all$mz)) ## obtaining the intensities of the all imp mz 
   imp_var<-column_to_rownames(imp_var,"mz")
   imp_var_t<-as.data.frame(t(imp_var))
   imp_var_t<-rownames_to_column(imp_var_t,"samples")
   imp_var_t$samples<-as.factor(ifelse(grepl(imp_var_t$samples, pattern = class1), class1, class2))
-  training_set<-imp_var_t
-
-
-  ## for test set ## what we do here is we make the test set with same selected imp peaks so we can test the final model
-
-
-  test_set$samples<-paste(test_set$samples,1:nrow(test_set),sep="_")
-  rownames(test_set)<-NULL
-  test_set<-column_to_rownames(test_set,"samples")
-
-  test_set_t<-as.data.frame(t(test_set))
-
-  test_set_t<-rownames_to_column(test_set_t,"mz")
-
-  test_set_t<-test_set_t %>% filter_all(any_vars(. %in% imp_all$mz))
-  test_set_t<-column_to_rownames(test_set_t,"mz")
-  test_set<-as.data.frame(t(test_set_t))
-  test_set<-rownames_to_column(test_set,"samples")
-  test_set$samples<-as.factor(ifelse(grepl(test_set$samples, pattern = class1), class1, class2))
-
-
-
-  colnames(training_set)<-as.character(colnames(training_set))
-
-  return(list("training_set"=training_set,"test_set"=test_set,"auc_all"=auc_all,"roc_all"=roc_all,"imp"=imp,
-              "cfmatrix_summary"=cfmatrix_summary))
+  data<-imp_var_t
+  colnames(data)<-as.character(colnames(data))
+  return(data)
 }
-
-
 
 ## the final model
 
