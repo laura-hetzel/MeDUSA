9e6048609c0fdc7a892a345498e9c6eed7b23e82
NotEric <ericmhetzel@gmail.com>
2023-08-31 23:28:02 +0200

    bug fixes

--- sqrlSumr/R/mz_2_subtraction.R
+++ sqrlSumr/R/mz_2_subtraction.R
@@ -43,8 +43,8 @@ mz_subtraction <- function(input_mz_obj, samples, compare_samples , threshold = 
     (input_mz_obj > compare) * input_mz_obj
   }
   samples_subset <- as.data.frame(sapply(samples_subset, .applyer))
-  row.names(samples_subset) <- row.names(input_mz_obj)
-  out_mz <- samples_subset[rowSums(samples_subset) > 0,]
+  samples_subset$mz <- input_mz_obj$mz
+  out_mz <- samples_subset[rowSums(dplyr::select(samples_subset,-mz)) > 0,]
   local.mz_log_removed_rows(input_mz_obj,out_mz,"sqrlSumr::mz_subtraction")
   out_mz
 }
