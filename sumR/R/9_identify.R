a03d85cda025ed56679c734fc4903ddeeac0cd4b
NotEric <ericmhetzel@gmail.com>
2023-10-29 03:15:27 +0100

    hmdb identify

--- sumR/R/9_identify.R
+++ sumR/R/9_identify.R
@@ -13,11 +13,26 @@
 #'   String: Location of hmdb.xml download file
 #'
 #' @export
-identify_hmdb <- function( mzs, adduct, hmdb_file = "/home/rstudio/hmbd_metabolites.xml" ) {
-  adducts <- c( "H"   = 1.0008 ,
-                "NH4" = 17.03052  )
-  hmdb <- XML::xmlParse("/home/rstudio/hmbd_metabolites.xml")
-
-  #TODO, draw the rest of the owl
-
+identify_hmdb <- function( mzs, adducts = 0 , hmdb_file = "/home/rstudio/local/hmdb_xml.Rdata", tolerance = 5e-6) {
+  if (class(adducts) == "character"){
+    common_adducts <- data.frame( "name" = c("H","NH4","K"),
+                                  "mz"   = c(1.0008, 17.03052 ,39.0983 ))
+    adducts = common_adducts$mz[common_adducts$name %in% adducts]
+  }
+  #xmlToDataFrame takes ~15mim.
+  if (grep("Rdata$",hmdb_file)){
+    load(hmdb_file)
+  } else {
+    hmdb <- XML::xmlToDataFrame(hmdb_file)
+    hmdb$monisotopic_molecular_weight <- as.numeric(hmdb$monisotopic_molecular_weight)
+    hmdb <- hmdb[!is.na(hmdb$monisotopic_molecular_weight),]
+  }
+  mz_expanded = unlist(lapply(mzs ,function(x) x - adducts))
+  print(mz_expanded)
+  cl <- local.export_thread_env(2,environment())
+  out <- lapply(mz_expanded, function(x) 
+    hmdb[abs(hmdb$monisotopic_molecular_weight - x)/x < tolerance ,]
+  )
+  local.kill_threads(cl)
+  do.call(rbind,out)
 }
--- sumR/tests/testthat/test-9_identify.R
+++ sumR/tests/testthat/test-9_identify.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# ============
+# identify_hmdb(mzs, adducts, hmdb_file)
+# ============
+test_that("identfy_hmdb: with known adducts", {
+  mz <- c(295.1449, 301.2163)
+  ad <- c("H","K")
+  load("../test_data/asserts/hmdb_filtered.Rdata")
+  actual <- identify_hmdb(mz,ad)
+	expect_identical(actual, hmdb_filtered)
+})
+
+
+test_that("identfy_hmdb: with custom adducts", {
+  mz <- c(394.1440, 262.4181)
+  ad <- c(100.0001, 0.3001)
+  load("../test_data/asserts/hmdb_filtered.Rdata")
+  actual <- identify_hmdb(mz,ad)
+	expect_identical(actual, hmdb_filtered)
 })
