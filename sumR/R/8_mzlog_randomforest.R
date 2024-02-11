812d4ae1462b3da20f651076155abc838a542aef
NotEric <ericmhetzel@gmail.com>
2024-02-11 23:45:20 +0100

    finished rf content, needs testing,

--- sumR/R/8_mzlog_randomforest.R
+++ sumR/R/8_mzlog_randomforest.R
@@ -30,18 +30,19 @@ mzlog_rf_correlation <- function(input_mzlog_obj, correlation_cutoff = 0.75){
 #'
 #' @export
 mzlog_rf_select <- function(correlation_data, metadata, attribute = "phenotype", feat_size_seq = seq(50,1000, by=50), plot = T) {
-  data <- dplyr::left_join(correlation_data, metadata[c("sample_name", attribute)], by="sample_name")
+  metadata <- local.meta_polarity_fixer(correlation_data, metadata)
+  data <- dplyr::left_join(correlation_data, meta[c("sample_name", attribute)], by="sample_name")
   att <- as.factor(data[[attribute]])
   control <- caret::rfeControl( functions = caret::rfFuncs,
                                 method = "repeatedcv",
                                 repeats = 5,
                                 number = 10)
 
-  feat_select <- caret::rfe(dplyr::select(data, -"sample_name", -attribute),
+  feat_select <- caret::rfe(dplyr::select(data, -sample_name, -attribute),
                             att,
                             rfeControl = control,
                             sizes = feat_size_seq )
-                            
+
   if(plot){
     ggplot(data = feat_select, metric = "Accuracy") + theme_bw()
     local.save_plot(paste("RandomForest Accuracy",local.mz_polarity_guesser(correlation_data),sep="-"))
@@ -51,7 +52,8 @@ mzlog_rf_select <- function(correlation_data, metadata, attribute = "phenotype",
   feat_select
 }
 
-# *** RandomForest Train -----------------------------------------------------
+
+# *** RandomForest model -----------------------------------------------------
 #' Train you model data within a mzLog_obj
 #'
 #' @param mzlog_obj \cr
@@ -63,16 +65,17 @@ mzlog_rf_select <- function(correlation_data, metadata, attribute = "phenotype",
 #' @param feat_size_seq \cr
 #'   Sequence to find optimal "number_of_variables"
 #' @param attribute ##TODO Readd; currently only supports "phenotype" \cr
-#'   String: which metadata attribute to compare 
+#'   String: which metadata attribute to compare
 #' @param seeds \cr
 #'   List: which seeds to use. Also how many runs to do
 #' @param cores \cr
 #'   Int: can I haz multithreading
 #'
+#' @returns list(model, test, train)
 #'
 #' @export
-mzlog_rf <- function(mzlog_obj,  metadata, rfe_obj = NULL, rf_trees = NULL, attribute = "phenotype", master_seeds = c(42,24),
-                          seeds = c(201705,666,314159,1.05457, 998001), ratio = 0.8, cores = 4){
+mzlog_rf <- function(mzlog_obj,  metadata, rfe_obj = NULL, rf_trees = NULL, attribute = "phenotype", master_seeds = c(42,1701),
+                          seeds = c(201705, 623.1912, 314159, 1.05457, 10191), ratio = 0.8, cores = 4, plot = T){
   if (! is.null(rfe_obj)){
     mzlog_obj <- mzlog_obj[ mzlog_obj$mz %in% as.numeric(caret::predictors(rfe_obj)), ]
   }
@@ -82,63 +85,100 @@ mzlog_rf <- function(mzlog_obj,  metadata, rfe_obj = NULL, rf_trees = NULL, attr
     }
     rf_trees <- rfe_obj$bestSubset
   }
-  
+  metadata <- local.meta_polarity_fixer(mzlog_obj, metadata)
+  pol <- local.mz_polarity_guesser(mzlog_obj)
   rownames(mzlog_obj) <- mzlog_obj$mz
   data_t <- data.frame(t(dplyr::select(mzlog_obj,-mz)))
   data_t <- tibble::rownames_to_column(data_t,"sample_name")
   data_t <- dplyr::left_join(data_t, metadata[c("sample_name", attribute)])
   data_t <- dplyr::select(data_t,-"sample_name")
   data_t[[attribute]] <- as.factor(data_t[[attribute]])
-  
+
   #Train settings
   mtry <- c(sqrt(ncol(data_t)))
   tunegrid <- expand.grid(.mtry=mtry)
-  control <- trainControl(method ='repeatedcv', 
-                          number = 10, 
-                          repeats = 4, 
+  control <- trainControl(method ='repeatedcv',
+                          number = 10,
+                          repeats = 4,
                           search = 'grid',
                           allowParallel = TRUE)
-  
-  master_split <- .seed_splitter(data_t,attribute,master_seed[1],ratio)
-  
-  cl <- local.export_thread_env(cores, environment(mzlog_rf_train))
+
+  master_split <- rf.seed_splitter(data_t,attribute,master_seeds[1],ratio)
+
+  #TODO, better manage variables among threads
+  rf_vars <- list("master_split" = master_split, "ratio" = ratio,
+                  "rf_trees" = rf_trees, "attribute" = attribute, "pol" = pol,
+                  "mtry"= mtry, "tunegrid" = tunegrid, "control" = control)
+
+  cl <- local.export_thread_env(cores, environment())
   tryCatch({
-    out <- pbapply::pblapply(seeds, cl=cl, .run_train)
+    out <- pbapply::pblapply(seeds, cl=cl, rf.run_train, rf_vars = rf_vars)
     out <- do.call(rbind,out)
     #plot ImpVars
   }, finally={
     local.kill_threads(cl)
   })
   out
-  
+
   ##Validate
-  train <- .transpose_massager(master_split$train, attribute)
-  test <- .transpose_massager(master_split$test, attribute)
+  train <- rf.transpose_massager(master_split$train, attribute)
+  test <- rf.transpose_massager(master_split$test, attribute)
 
-
-  set.seed(master_seed[2])
-  model <- randomForest(x = train_neg[-1],
-                        y = train_neg[[attribute]],
-                        data = train_neg,
-                        ntree = rf_tree,
+  set.seed(master_seeds[2])
+  model <- randomForest(x = master_split$train[-1],
+                        y = master_split$train[[attribute]],
+                        data = master_split$train,
+                        ntree = rf_trees,
                         mtry = mtry,
-                        importance = TRUE, 
-                            proximity = TRUE)
+                        importance = TRUE,
+                        proximity = TRUE)
+
+  distance_matrix <- as.dist(1 - model$proximity)
+  mds <- cmdscale(distance_matrix, eig = TRUE, x.ret = TRUE)
+  mds_var <- round(mds$eig/sum(mds$eig)*100, 1)
+  mds_values <- mds$points
+  mds_data <- data.frame(Sample = rownames(mds_values),
+                             x = mds_values[,1],
+                             y = mds_values[,2],
+                             Phenotype = master_split$train$phenotype)
+  if(plot){
+    plot(model)
+    local.save_plot(paste0("rf_model_",pol))
+
+    varImpPlot(model, col = "Blue", pch = 2, main = paste("Important Variables ",pol))
+    local.save_plot(paste0("rf_varImp_",pol))
+    pheno <- as.vector(unique(master_split$train$phenotype))
+    ggplot(mds_xxdata, aes(x = x, y = y, label = Sample)) +
+      geom_point(aes(color = Phenotype), size = 3) +
+      theme_classic() +
+      theme(legend.position = "bottom",) +
+      theme(text = element_text(size = 18)) +
+      xlab(paste("MDS1 - ", mds_var_neg[1], "%", sep = "")) +
+      ylab(paste("MDS2 - ", mds_var_neg[2], "%", sep = "")) +
+      scale_color_manual(breaks = pheno,
+                         values = c("red4", "deepskyblue2")) +
+      ggtitle(paste0("MDS plot ", pol," using random forest proximities"))
+      local.save_plot(paste0("rf_mds_",pol))
+
+  }
+
+  list( model = model, test = master_split$test, train = master_split$train )
 }
 
-.transpose_masssager <- function(data, attribute){
+
+rf.transpose_massager <- function(data, attribute){
   rand <- sample(1:nrow(data),nrow(data))
   data[[attribute]] <- paste(data[[attribute]], rand, sep="_")
   rownames(data) <- NULL
-  data <- column_to_rownames(data, attribute)
-  
+  data <- tibble::column_to_rownames(data, attribute)
+
   data <- as.data.frame(t(data))
-  data <- rownames_to_column(data, "mz")
+  data <- tibble::rownames_to_column(data, "mz")
   data$mz <- as.numeric(gsub("X","",data$mz))
   data
 }
 
-.seed_splitter <- function(data,attribute,seed, ratio){
+rf.seed_splitter <- function(data, attribute, seed, ratio){
   set.seed(seed)
   split <- caTools::sample.split(data[[attribute]], SplitRatio = ratio)
   tr <- subset(data, split == T)
@@ -146,30 +186,31 @@ mzlog_rf <- function(mzlog_obj,  metadata, rfe_obj = NULL, rf_trees = NULL, attr
   list(train = tr, test = te)
 }
 
-.run_train <- function( seed, master_split, ratio, rf_trees, attribute, mtry, tunegrid, control){
+rf.run_train <- function( seed, rf_vars){
   #Split
-  split <- .seed_splitter(master_split$train, attribute, seed, ratio)
+  split <- rf.seed_splitter(rf_vars$master_split$train, rf_vars[[attribute]], seed, rf_vars$ratio)
   #TODO Dumb workaround to make "as.factor()" work: (it doesn't like parameters)
   tmp <- split$train
-  colnames(tmp)[colnames(tmp) == attribute] <- "llama"  
+  colnames(tmp)[colnames(tmp) == rf_vars$attribute] <- "factor"
   #Train
-  rf_fit <- caret::train( as.factor(llama) ~.,
+  rf_fit <- caret::train( as.factor(factor) ~.,
                    data = tmp,
                    method = 'rf',
-                   tuneGrid = tunegrid,
-                   trControl = control,
-                   ntree = rf_trees ,
+                   tuneGrid = rf_vars$tunegrid,
+                   trControl = rf_vars$control,
+                   ntree = rf_vars$rf_trees ,
                    na.action = na.exclude)
-  
-  rf_pred <- predict(rf_fit, split$test)
-  acc <- caret::confusionMatrix(rf_pred, as.factor(split$test[[attribute]]))$overall
-  #TODO
-  # polarity guesser
-  # AOC
-  # ROC
-  # Accuracty
+
+  rf_pred <- predict(rf_fit, split$train$phenotype )
+  acc <- caret::confusionMatrix(rf_pred, as.factor(split$test[[rf_vars$attribute]]))$overall
+  roc <- pROC::roc(tmp$phenotype, rf_pred)
+  auc <- pROC::auc(roc)
+  plot(roc, col = "Red", main = paste0("Fold_",pol,"_", seed),
+       sub = paste0("Acc:",acc," AUC:", as.character(round(auc, 3))))
+  local.save_plot(paste0("rf_train_",seed))
+
   imp <- caret::varImp(rf_fit)$importance
-  imp <- rownames_to_column(imp, "mz")
+  imp <- tibble::rownames_to_column(imp, "mz")
   imp$mz <- as.numeric(gsub("X", "", imp$mz))
   imp <- head(imp[order(-imp$Overall),], n=100)
   rownames(imp) <- NULL
---
+++ sumR/R/8_rf_validation.R
@@ -0,0 +1,81 @@
+
+# *** RandomForest Validate-----------------------------------------------------
+#' Train you model data within a mzLog_obj
+#'
+#' @param rf_obj \cr
+#'   List : from mzlog_rf: list(model, test, train)
+#' @param mtry_seed \cr
+#'   List: which seed to use.
+#' @param cores \cr
+#'   Int: can I haz multithreading
+#'
+#'
+#' @export
+rf_validate <- function(rf_obj, attribute = "phenotype", mtry_range = c(1:200), mtry_seed = 1984){
+  pred_train <- rf.predict(rf_obj$model, rf_obj$train)
+  pred_test <- rf.predict(rf_obj$model, rf_obj$test)
+  results <- as.data.frame(cbind("actual" = rf_obj$test$phenotype,
+                                 "prediction" = pred_test))
+
+  ml <- data.frame(mtry = mtry_range)
+  cl <- local.export_thread_env(cores, environment())
+  tryCatch({
+   ml$err_rate <- pbapply::pbapply(ml, 1, rf.mtry_fit, cl = cl,
+                                   data = rf_obj$train, seed = mtry_seed)
+   best_mtry <- modellist[ml$err_rate == min(ml$err_rate),]
+   print(paste0("INFO: sumR::rf_validate: Lowest error:", best_mtry$err_rate,
+                " At mtry:", best_mtry$mtry ))
+  }, finally={
+   local.kill_threads(cl)
+  })
+
+  list(train_confusionMatrix <- pred_train$cm, test_confusionMatrix <- pred_test$cm, mtry_error <- ml)
+}
+
+
+# *** RandomForest Permuted -----------------------------------------------------
+#' Train you model data within a mzLog_obj
+#'
+#' @param rf_obj \cr
+#'   List : from mzlog_rf: list(model, test, train)
+#' @param mtry_seed \cr
+#'   List: which seed to use.
+#' @param cores \cr
+#'   Int: can I haz multithreading
+#'
+#'
+#' @export
+rf_permuted <- function(rf_obj, attribute = "phenotype", seed = 105.9){
+  permuted <- cbind(rf_obj$train, rf_obj$test)
+  attr <- as.vector(unique(permuted$phenotype))
+  set.seed(seed)
+  false_samples <- sample(attr, nrow(permuted), replace = TRUE)
+
+  permuted$phenotype <- as.factor(false_samples)
+  pred_permuted <- rf.predict(rf_obj$model, permuted)
+
+  results_permuted<- as.data.frame(cbind("actual" = permuted_pos$phenotype,
+                                              "prediction" = pred_permuted$pred))
+  roc_permuted <- roc(results_permuted$actual, results_permuted$prediction)
+  auc_permuted <- auc(roc_permuted)
+
+  list(permuted_confusionMatrix <- pred_permuted$cm)
+}
+
+rf.mtry_fit <- function(mtry, data, seed){
+  set.seed(seed)
+  fit <- randomForest::randomForest(x = ( dplyr::select(data, -phenotype)),
+                      y = data$phenotype,
+                      data = data,
+                      ntree = 500,
+                      mtry = mtry,
+                      importance = TRUE)
+  fit[["err.rate"]][nrow(fit[["err.rate"]]),1]
+}
+
+rf.predict <- function (model, data){
+  data$phenotype <- as.factor(data$phenotype)
+  pred <- predict(model, newdata = data)
+  cm <- confusionMatrix(data = pred, reference = data$phenotype)
+  list(pred = pred, cm = cm)
+}
