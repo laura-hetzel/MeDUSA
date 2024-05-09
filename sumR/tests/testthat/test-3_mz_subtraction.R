09fb275b2112e83ab8274a3d1f6c35a5296fd319
NotEric <ericmhetzel@gmail.com>
2024-05-09 14:44:51 +0200

    finished subtract tests

--- sumR/tests/testthat/test-3_mz_subtraction.R
+++ sumR/tests/testthat/test-3_mz_subtraction.R
@@ -5,10 +5,19 @@ test_that("mz_subtraction: Happy path",{
   load("../test_data/mz_neg.Rdata")
   load("../test_data/asserts/mz_subtraction.Rdata")
   meta <- data.frame(read.csv("../test_data/meta_neg.csv"))
-  actual <- mz_subtraction(mztools_filter(mz_neg, meta,"IS_Blank","type",T,T),
-                           mztools_filter(mz_neg, meta,"IS_Blank","type",F,T))
-                           
+  actual <- mz_subtraction(mztools_filter(mz_neg, meta,"red","phenotype",T,T),
+                           mztools_filter(mz_neg, meta,"red","phenotype",F,T))
   expect_identical(actual,mz_subtraction)
-  
 })
 
+test_that("mz_subtraction: Custom params",{
+  load("../test_data/mz_neg.Rdata")
+  load("../test_data/asserts/mz_subtract_custom.Rdata")
+  math <- function(x){mean(x)^22}
+  meta <- data.frame(read.csv("../test_data/meta_neg.csv"))
+  actual <- mz_subtraction(mztools_filter(mz_neg, meta,"red","phenotype",T,T),
+                           mztools_filter(mz_neg, meta,"red","phenotype",F,T),
+                           method = math, threshold = 2)
+
+  expect_identical(actual,mz_subtract_custom)
+})
